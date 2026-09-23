dc207e43374f1f0a5a7abea0a417c771439c5871
salman178600 <hajerabegum9381188200@gmail.com>
2026-09-23 08:30:25 +0530

    Update App.tsx

--- source/src/App.tsx
+++ source/src/App.tsx
@@ -288,7 +288,7 @@ export default function App() {
         {/* 2-Column Responsive Workspace */}
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 gap-y-4 flex-1 min-h-0 overflow-hidden">
           {/* Left / Main Column: Live Customer Chat Interface */}
-          <div className="lg:col-span-7 xl:col-span-8">
+          <div className="lg:col-span-7 xl:col-span-8 h-full min-h-0">
             <ChatInterface
               messages={messages}
               isLoading={isLoading}
